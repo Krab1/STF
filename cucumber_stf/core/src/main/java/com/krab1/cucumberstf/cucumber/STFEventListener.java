1b06f48b6a0fbeb838decfcc4b2433d0654bec3e
froyo <kolya-200587@mail.ru>
2021-06-29 20:58:42 +0200

    Fixing test task for subproject_test small update to gradle builds and report generation

--- cucumber_stf/core/src/main/java/com/krab1/cucumberstf/cucumber/STFEventListener.java
+++ cucumber_stf/core/src/main/java/com/krab1/cucumberstf/cucumber/STFEventListener.java
@@ -5,6 +5,13 @@ import io.cucumber.plugin.event.EventHandler;
 import io.cucumber.plugin.event.EventPublisher;
 import io.cucumber.plugin.event.TestRunFinished;
 import io.cucumber.plugin.event.TestRunStarted;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.json.support.Status;
+import net.masterthought.cucumber.presentation.PresentationMode;
+
+import java.io.File;
+import java.util.Collections;
 
 public class STFEventListener implements ConcurrentEventListener {
 
@@ -17,16 +24,16 @@ public class STFEventListener implements ConcurrentEventListener {
 
     private EventHandler<TestRunFinished> generateReport() {
         return event -> {
-//            File reportOutputDir = new File("target");
-//            Configuration configuration = new Configuration(reportOutputDir, "cucumberProject");
-//            configuration.addPresentationModes(PresentationMode.RUN_WITH_JENKINS);
-//            configuration.setNotFailingStatuses(Collections.singleton(Status.SKIPPED));
-//            configuration.setBuildNumber("1");
-//            configuration.addClassifications("Platform", "Windows");
-//            configuration.addClassifications("Browser", "Chrome");
-//            configuration.addClassifications("Branch", "release/1.0");
-//            configuration.addPresentationModes(PresentationMode.PARALLEL_TESTING);
-//            new ReportBuilder(Collections.singletonList("target/report-cucumber.json"), configuration).generateReports();
+            File reportOutputDir = new File("target");
+            Configuration configuration = new Configuration(reportOutputDir, "cucumberProject");
+            configuration.addPresentationModes(PresentationMode.RUN_WITH_JENKINS);
+            configuration.setNotFailingStatuses(Collections.singleton(Status.SKIPPED));
+            configuration.setBuildNumber("1");
+            configuration.addClassifications("Platform", "Windows");
+            configuration.addClassifications("Browser", "Chrome");
+            configuration.addClassifications("Branch", "release/1.0");
+            configuration.addPresentationModes(PresentationMode.PARALLEL_TESTING);
+            new ReportBuilder(Collections.singletonList("target/report-cucumber.json"), configuration).generateReports();
         };
     }
 
--- cucumber_stf/subproject_test/src/test/java/com/krab1/cucumberstf/TestRunner.java
+++ cucumber_stf/subproject_test/src/test/java/com/krab1/cucumberstf/TestRunner.java
@@ -3,7 +3,7 @@ package com.krab1.cucumberstf;
 import io.cucumber.junit.platform.engine.Cucumber;
 
 @Cucumber
-public class MyRunner {
-    private MyRunner(){
+public class TestRunner {
+    private TestRunner(){
     }
 }
